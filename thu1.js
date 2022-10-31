3e10b95fcccf11d479c73ca75f83b7acf5eec246
dangtung <tungdang1997@example.com>
2022-10-31 17:25:14 +0700

    baithi

---
+++ thu1.js
@@ -0,0 +1,9 @@
+function tinhToan(){
+let ghang = document.getElementById('giohang');
+let dachonhang = document.getElementById('amount');
+let tinh= 0;
+for( let ip of dachonhang){
+    tinh += ip;
+}
+let ketqua = document.getElementById('total').innerText;
+}
